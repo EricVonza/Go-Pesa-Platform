4f5d455df923ced1f58860c7cc60f9eeb895a6f7
Eric Vonza <ericvonza47@gmail.com>
2025-02-18 03:04:23 +0300

    Added Landing Page Content

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import Home from './pages/Home';
+import Motivational_Section from './pages/Motivational_Section';
 import NavBar from './components/NavBar';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <NavBar />
     <Home />
+    <Motivational_Section />
   </StrictMode>
 );
 
