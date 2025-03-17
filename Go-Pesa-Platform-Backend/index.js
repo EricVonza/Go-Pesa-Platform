efc47205f852614169012620d072a789e14e6f49
Eric Vonza <ericvonza47@gmail.com>
2025-03-17 11:02:45 +0300

    Added backend

---
+++ Go-Pesa-Platform-Backend/index.js
@@ -0,0 +1,15 @@
+const   express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+const connectDB = async ()=>{
+
+    mongoose.connect("mongodb://localhost:27017/go-pesa");
+    const productSchema = new mongoose.Schema({});
+    const product = mongoose.model("products", productSchema)
+    const data = await product.find();
+    console.log(data);
+}
+
+
+app.listen(5000);
