dcc0c32f422f92b774e37fdc06a261c9a215c076
Eric Vonza <ericvonza47@gmail.com>
2025-07-03 11:58:58 +0300

    Added remote MongoDB parameters

--- Go-Pesa-Platform-BackEnd/src/app.js
+++ Go-Pesa-Platform-BackEnd/src/app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 
 // MongoDB Connection
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
